1ca9add4b83690b620d43ec15b7060c008864d8c
Sebastian Barahona <sebastian.barahona@mail.escuelaing.edu.co>
2026-09-04 13:31:06 -0500

    Mejora la salida con precision, contador de iteraciones y verificacion f(raiz)

--- C_C++/raices/main_SebastianBarahona.cpp
+++ C_C++/raices/main_SebastianBarahona.cpp
@@ -3,13 +3,15 @@
 #include <cmath>
 #include <gsl/gsl_roots.h>
 #include <gsl/gsl_errno.h>
+#include <string>
+#include <iomanip>
 
 // f(x) = x^3 - 5x + 1
 double f(double x, void *) {
   return x*x*x - 5.0*x + 1.0;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   const gsl_root_fsolver_type *T;
   gsl_root_fsolver *s;
   gsl_function F;
@@ -17,7 +19,19 @@ int main() {
   F.params = nullptr;
   double x_lo = 0.0;
   double x_hi = 1.0;
-  T = gsl_root_fsolver_bisection;
+
+  std::string metodo = (argc > 1) ? argv[1] : "bisection";
+
+  if (metodo == "bisection")      T = gsl_root_fsolver_bisection;
+  else if (metodo == "falsepos")  T = gsl_root_fsolver_falsepos;
+  else if (metodo == "brent")     T = gsl_root_fsolver_brent;
+  else {
+    std::cerr << "Metodo no valido: " << metodo << "\n";
+    std::cerr << "Use: bisection | falsepos | brent\n";
+    return 1;
+  }
+
+  std::cout << "Metodo: " << metodo << "\n";
   s = gsl_root_fsolver_alloc(T);
   gsl_root_fsolver_set(s, &F, x_lo, x_hi);
   std::cout << "iter\t" << "inf\t" << "sup\t" << "raíz\n";
@@ -37,7 +51,10 @@ int main() {
           status = gsl_root_test_interval( x_lo, x_hi, 0.0, 1e-8);
   } while(status == GSL_CONTINUE && iter < max_iter);
 
-  std::cout << "\nRaiz encontrada = " << r << std::endl; gsl_root_fsolver_free(s);
-  return 0;
+  std::cout << "\nMetodo          = " << metodo << "\n";
+  std::cout << "Raiz encontrada = " << std::fixed << std::setprecision(10) << r << "\n";
+  std::cout << "Iteraciones     = " << iter << "\n";
+  std::cout << "f(raiz)         = " << std::scientific << f(r, nullptr) << "\n"; 
+ return 0;
 }
 
